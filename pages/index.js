ebd7b249f72cb12399c79354546355b3728fd023
Chris Sevilleja <chris@scotch.io>
2019-08-29 09:27:33 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import Link from 'next/link';
 import Head from 'next/head';
-import Nav from '../components/Nav';
+import Nav from '../components/nav';
 import '../assets/styles/main.css';
 
 const Home = () => (
